351d3c7f377ef7cac662b7e9780570e5f2e89d6a
emir <emirkartal00@gmail.com>
2024-05-03 07:33:37 +0300

    feat: :building_construction: added base project architecture

---
+++ src/app/pages/verify-password/verify-password.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-verify-password',
+  standalone: true,
+  imports: [],
+  templateUrl: './verify-password.component.html',
+  styleUrl: './verify-password.component.css'
+})
+export class VerifyPasswordComponent {
+
+}
